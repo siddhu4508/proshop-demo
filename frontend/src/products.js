6fc89766296129d097884eab132aeb2e88e2874d
Siddartha Yadav Sangam Reddy <siddhuyadav508@gmail.com>
2025-04-01 14:40:36 +0530

    images & HomeScreen Products

---
+++ frontend/src/products.js
@@ -0,0 +1,66 @@
+const products = [
+  {
+    _id: "1",
+    name: "Airpods Wireless Bluetooth Headphones",
+    image: "../public/images/airpods.jpg",
+    description:
+      "Introducing the ultimate in headphones. With its powerful wireless technology and vibrant blue color, AirPods deliver unparalleled sound quality and comfort.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "iPhone 11 Pro 256GB Memory",
+    image: "../public/images/phone.jpg",
+    description:
+      "The iPhone 11 Pro is the most advanced smartphone Apple has ever made. With a powerful A13 Bionic chip, a triple-camera system, and a stunning Super Retina XDR display, the iPhone 11 Pro is the perfect device for anyone looking to take their mobile experience to the next level.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "Cannon EOS 80D DSLR Camera",
+    image: "../public/images/camera.jpg",
+    description:
+      "The Canon EOS 80D is a powerful DSLR camera that offers advanced features and high-quality images. With a 24.2 megapixel CMOS sensor and DIGIC 6 image processor, the EOS 80D delivers stunning photos and videos in any situation.",
+    brand: "Cannon",
+    category: "Electronics",
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Sony Playstation 4 Pro White Version",
+    image: "../public/images/playstation.jpg",
+    description:
+      "The Sony PlayStation 4 Pro is the ultimate gaming console. With 4K gaming capabilities and a powerful processor, the PS4 Pro delivers stunning graphics and performance that will take your gaming experience to the next level.",
+    brand: "Sony",
+    category: "Electronics",
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: "5",
+    name: "Logitech G-Series Gaming Mouse",
+    image: "../public/images/mouse.jpg",
+    description:
+      "Get the ultimate gaming experience with the Logitech G-Series Gaming Mouse. With advanced features and a sleek design, the G-Series Gaming Mouse is the perfect accessory for any gamer looking to take their skills to the next level.",
+    brand: "Logitech",
+    category: "Electronics",
+    price: 49.99,
+  },
+];
+
+export default products;
