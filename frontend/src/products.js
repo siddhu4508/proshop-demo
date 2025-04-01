b8657548a00a4e643c38dc211c2128d1be25c9e1
Siddartha Yadav Sangam Reddy <siddhuyadav508@gmail.com>
2025-04-01 17:46:37 +0530

    Added ProductScreen, Details Page

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -2,7 +2,7 @@ const products = [
   {
     _id: "1",
     name: "Airpods Wireless Bluetooth Headphones",
-    image: "../public/images/airpods.jpg",
+    image: "/images/airpods.jpg",
     description:
       "Introducing the ultimate in headphones. With its powerful wireless technology and vibrant blue color, AirPods deliver unparalleled sound quality and comfort.",
     brand: "Apple",
@@ -15,7 +15,7 @@ const products = [
   {
     _id: "2",
     name: "iPhone 11 Pro 256GB Memory",
-    image: "../public/images/phone.jpg",
+    image: "/images/phone.jpg",
     description:
       "The iPhone 11 Pro is the most advanced smartphone Apple has ever made. With a powerful A13 Bionic chip, a triple-camera system, and a stunning Super Retina XDR display, the iPhone 11 Pro is the perfect device for anyone looking to take their mobile experience to the next level.",
     brand: "Apple",
@@ -27,11 +27,11 @@ const products = [
   },
   {
     _id: "3",
-    name: "Cannon EOS 80D DSLR Camera",
-    image: "../public/images/camera.jpg",
+    name: "Canon EOS 80D DSLR Camera",
+    image: "/images/camera.jpg",
     description:
       "The Canon EOS 80D is a powerful DSLR camera that offers advanced features and high-quality images. With a 24.2 megapixel CMOS sensor and DIGIC 6 image processor, the EOS 80D delivers stunning photos and videos in any situation.",
-    brand: "Cannon",
+    brand: "Canon",
     category: "Electronics",
     price: 929.99,
     countInStock: 5,
@@ -41,7 +41,7 @@ const products = [
   {
     _id: "4",
     name: "Sony Playstation 4 Pro White Version",
-    image: "../public/images/playstation.jpg",
+    image: "/images/playstation.jpg",
     description:
       "The Sony PlayStation 4 Pro is the ultimate gaming console. With 4K gaming capabilities and a powerful processor, the PS4 Pro delivers stunning graphics and performance that will take your gaming experience to the next level.",
     brand: "Sony",
@@ -54,12 +54,15 @@ const products = [
   {
     _id: "5",
     name: "Logitech G-Series Gaming Mouse",
-    image: "../public/images/mouse.jpg",
+    image: "/images/mouse.jpg",
     description:
       "Get the ultimate gaming experience with the Logitech G-Series Gaming Mouse. With advanced features and a sleek design, the G-Series Gaming Mouse is the perfect accessory for any gamer looking to take their skills to the next level.",
     brand: "Logitech",
     category: "Electronics",
     price: 49.99,
+    countInStock: 0, // Explicitly stating out of stock
+    rating: 4.2,
+    numReviews: 5,
   },
 ];
 
