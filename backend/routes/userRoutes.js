758c835c2318b5145269a609e39c3b8e62a3c83e
Siddartha Yadav Sangam Reddy <siddhuyadav508@gmail.com>
2025-04-04 10:15:44 +0530

    User Register Profile Endpoints Encryption

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -19,7 +19,7 @@ router.post("/logout", logoutUser);
 
 router.post("/auth", authUser);
 
-router.route("/profile").get(getUserProfile).put(updateUserProfile);
+router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile);
 
 router.route("/:id").delete(protect, admin,deleteUser).get(protect, admin, getUserByID).put(protect, admin, updateUser);
 
